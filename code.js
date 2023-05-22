19cfc46180d75be9aa31c4d531f5b9c5fc0ead1d
MightySleep <jaredhenrysleep@gmail.com>
2023-05-21 19:21:14 -0600

    Finished project. Has multiple color selection, free resolution selection.

--- code.js
+++ code.js
@@ -1,13 +1,46 @@
-function makeSolid()
+let COLOR = "Black"
+function testFunction(number)
 {
-   alert("Good work");
+    
+    RESOLUTION = Number(number);
+
+    while(container.hasChildNodes())
+    {
+        container.removeChild(container.firstChild);
+    }
+    
+    strng = "repeat( " + number + ",auto)";
+    container.style.gridTemplateRows = strng;
+    container.style.gridTemplateColumns = strng;
+
+    for(let i=0;i<(number*number);i++)
+    {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        container.appendChild(cell);
+    }
+    const cells = document.querySelectorAll('.cell');
+
+    for(let i = 0; i<cells.length;i++)
+    {
+        
+        // cells[i].addEventListener('mouseover',makeSolid);
+        cells[i].addEventListener('mouseover', function (e) {
+            e.target.style.background = COLOR;
+        });
+
+    }
+ 
 }
-
-
+let RESOLUTION = 0;
 const container = document.querySelector('.container');
 container.style.gridTemplateRows = "repeat(6,6fr)"
 container.style.gridTemplateColumns = "repeat(6,6fr)"
-document.getElement
+const input_box = document.getElementById("input_box");
+input_box.addEventListener('change', function(e) {
+   testFunction(e.target.value);
+});
+
 
 for(let i=0;i<36;i++)
 {
@@ -19,95 +52,18 @@ for(let i=0;i<36;i++)
 const btn_4 = document.querySelector('#b4');
 btn_4.addEventListener('click', () => {
     
-    while(container.hasChildNodes())
-    {
-        container.removeChild(container.firstChild);
-    }
-    alert("test");
-    
-    container.style.gridTemplateRows = "repeat(4,auto)"
-    container.style.gridTemplateColumns = "repeat(4,auto)"
-
-    for(let i=0;i<16;i++)
-    {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        container.appendChild(cell);
-    }
-    const cells = document.querySelectorAll('.cell');
-
-    for(let i = 0; i<cells.length;i++)
-    {
-        
-        // cells[i].addEventListener('mouseover',makeSolid);
-        cells[i].addEventListener('mouseover', function (e) {
-            e.target.style.background = 'pink';
-        });
-
-    }
+    COLOR = "green";
 
 });
 
 const btn_6 = document.querySelector('#b6');
 btn_6.addEventListener('click', () => {
-    while(container.hasChildNodes())
-    {
-        container.removeChild(container.firstChild);
-    }
-    alert("test");
-    
-    container.style.gridTemplateRows = "repeat(6,auto)"
-    container.style.gridTemplateColumns = "repeat(6,auto)"
-
-    for(let i=0;i<36;i++)
-    {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        container.appendChild(cell);
-    }
-    const cells = document.querySelectorAll('.cell');
-
-    for(let i = 0; i<cells.length;i++)
-    {
-        
-        // cells[i].addEventListener('mouseover',makeSolid);
-        cells[i].addEventListener('mouseover', function (e) {
-            e.target.style.background = 'pink';
-        });
-
-    }
-
+    COLOR = "blue";
 });
 
 const btn_8 = document.querySelector('#b8');
 btn_8.addEventListener('click', () => {
-    while(container.hasChildNodes())
-    {
-        container.removeChild(container.firstChild);
-    }
-    alert("test");
-    
-    container.style.gridTemplateRows = "repeat(100,auto)"
-    container.style.gridTemplateColumns = "repeat(100,auto)"
-
-    for(let i=0;i<10000;i++)
-    {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        container.appendChild(cell);
-    }
-    const cells = document.querySelectorAll('.cell');
-
-    for(let i = 0; i<cells.length;i++)
-    {
-        
-        // cells[i].addEventListener('mouseover',makeSolid);
-        cells[i].addEventListener('mouseover', function (e) {
-            e.target.style.background = 'black';
-        });
-
-    }
-
+    COLOR = 'pink';
 });
 
 
@@ -119,7 +75,7 @@ for(let i = 0; i<cells.length;i++)
     
     // cells[i].addEventListener('mouseover',makeSolid);
     cells[i].addEventListener('mouseover', function (e) {
-        e.target.style.background = 'pink';
+        e.target.style.background = COLOR;
     });
 
 }
